17e73e0d45158ada93775bc4df6788cad00d340d
iraik <160371854+iraik@users.noreply.github.com>
2024-02-19 01:48:31 -0500

    removed .env and added to config.js

--- public/config.js
+++ public/config.js
@@ -1,9 +1,9 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "CHANGEME",
+  VITE_CORS_PROXY_URL: "https://roaring-pudding-6bc9ec.netlify.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZWFhYTI5Y2QyYjA4YzEwNDg1ZmYyNjgzYmUyY2M3MyIsInN1YiI6IjY1ZDI3NWU1MjhkN2ZlMDE2MzM0MzQ5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.x-ikEsv5qPIQi0_CDdd_WSSnpYy-koiESQcsQ1K_fSE",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
